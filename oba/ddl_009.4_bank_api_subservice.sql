d2c4b9c41c7ee1f199b74f7c52b5bd9b6fa43d32
Ruben <rubenski@gmail.com>
2020-06-09 14:27:18 +0200

    Name change and no more link between registration and service - not needed

--- oba/ddl_009.4_bank_api_subservice.sql
+++ oba/ddl_009.4_bank_api_subservice.sql
@@ -5,8 +5,8 @@ CREATE TABLE bank_api_subservice
 (
     system_name              char(50) NOT NULL,
     display_name             char(50) NOT NULL,
-    parent_available_service char(30) NOT NULL,
-    FOREIGN KEY (parent_available_service) REFERENCES bank_api_available_service (system_name) ON UPDATE CASCADE ON DELETE CASCADE,
+    parent_offered_service char(30) NOT NULL,
+    FOREIGN KEY (parent_offered_service) REFERENCES bank_api_offered_service (system_name) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (system_name)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
