b525c62f85e497bc728a70aa3616f6f41cee5a30
Ruben <rubenski@gmail.com>
2020-09-06 16:22:48 +0200

    Fix name duplication (Sabadell instead of Santander)

--- oba/dml_002_insert_banks.sql
+++ oba/dml_002_insert_banks.sql
@@ -14,6 +14,10 @@ VALUES (UUID_TO_BIN('cf32581c-c2f3-11ea-b3de-0242ac130004'), 'PSD2', 'OAUTH', tr
 INSERT INTO api(id, type, flow_type, request_signing_used, request_signing_algorithm, mutual_tls_used, is_sandbox, connection_per_account, status, multiple_redirects)
 VALUES (UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'PSD2', 'OAUTH', true, 'RSA_2048', true, true, false, 'ENABLED', false);
 
+# RedSys
+INSERT INTO api(id, type, flow_type, request_signing_used, request_signing_algorithm, mutual_tls_used, is_sandbox, connection_per_account, status, multiple_redirects)
+VALUES (UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'PSD2', 'OAUTH', true, 'RSA_2048', true, true, false, 'ENABLED', true);
+
 
 # Products
 INSERT INTO product(name) VALUES ('current_account');
@@ -23,8 +27,10 @@ INSERT INTO product(name) VALUES ('debit_card');
 INSERT INTO product(name) VALUES ('card');
 
 # Apis to products
+
+# Rabobak (double check)
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('ca80d196-a8cb-11ea-bb37-0242ac130002'), 'current_account');
-INSERT INTO api_2_product VALUES(UUID_TO_BIN('ca80d196-a8cb-11ea-bb37-0242ac130002'), 'credit_card');
+# INSERT INTO api_2_product VALUES(UUID_TO_BIN('ca80d196-a8cb-11ea-bb37-0242ac130002'), 'credit_card');
 
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('cf32581c-c2f3-11ea-b3de-0242ac130004'), 'current_account');
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('cf32581c-c2f3-11ea-b3de-0242ac130004'), 'credit_card');
@@ -32,6 +38,9 @@ INSERT INTO api_2_product VALUES(UUID_TO_BIN('cf32581c-c2f3-11ea-b3de-0242ac1300
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'current_account');
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'credit_card');
 INSERT INTO api_2_product VALUES(UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'debit_card');
+
+# RedSys  (supports current account only)
+INSERT INTO api_2_product VALUES(UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'current_account');
 
 # Country data provider
 INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
@@ -54,6 +63,30 @@ VALUES ('ing-bank-nl', 'ING Bank NL', UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242a
 
 INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
 VALUES ('ing-bank-fr', 'ING Bank Fr', UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'FRA', 'Europe/Paris', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('sabadell-es', 'Sabadell', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('santander-es', 'Santander', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('bankia-es', 'Bankia', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('bbva-es', 'BBVA', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('selfbank-es', 'Self Bank', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('evobanco-es', 'EVO Banco', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('caixa-es', 'Caixa', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
+
+INSERT INTO country_data_provider (system_name, display_name, api_id, country, time_zone, currency)
+VALUES ('bankinter-es', 'Bankinter', UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'ESP', 'Europe/Madrid', 'EUR');
 
 # Main service types
 INSERT INTO main_service(system_name, display_name)
@@ -98,6 +131,11 @@ VALUES (UUID_TO_BIN('cf32581c-c2f3-11ea-b3de-0242ac130004'), 'accounts-transacti
 # ING
 INSERT INTO api_2_subservice (api_id, subservice_system_name)
 VALUES (UUID_TO_BIN('da15593c-c2f3-11ea-b3de-0242ac130004'), 'accounts-transactions');
+
+# RedSys
+INSERT INTO api_2_subservice (api_id, subservice_system_name)
+VALUES (UUID_TO_BIN('ba34741c-ed4c-11ea-adc1-0242ac120002'), 'accounts-transactions');
+
 
 
 
