32cd4fdab18785292536cd0a54fe974802193a78
ruben <rubenski@gmail.com>
2020-10-13 21:19:16 +0200

    Added sandbox instruction field

--- oba/ddl_009.1_country_data_provider.sql
+++ oba/ddl_009.1_country_data_provider.sql
@@ -3,13 +3,15 @@ DROP TABLE IF EXISTS oba.api_provider;
 
 CREATE TABLE `country_data_provider`
 (
-    system_name  char(30)     NOT NULL,
-    display_name varchar(50)  NOT NULL,
-    api_id       binary(16)   NOT NULL,
-    country      char(3)      NOT NULL,
-    time_zone    varchar(30)  NOT NULL,
-    currency     char(3)      NOT NULL,
-    group_name   varchar(30)  NULL,
+    system_name                      char(30)     NOT NULL,
+    display_name                     varchar(50)  NOT NULL,
+    api_id                           binary(16)   NOT NULL,
+    country                          char(3)      NOT NULL,
+    time_zone                        varchar(30)  NOT NULL,
+    currency                         char(3)      NOT NULL,
+    bank_preferred_description_field char(20)     NOT NULL,
+    group_name                       varchar(30)  NULL,
+    sandbox_instructions_html        varchar(255) NULL,
     PRIMARY KEY (system_name),
     FOREIGN KEY (api_id) REFERENCES api (id) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB
